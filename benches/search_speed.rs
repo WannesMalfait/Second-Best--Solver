f3c0c0da7ec260c9a2276d150be146baf5347890
Wannes Malfait <wannes.malfait@gmail.com>
2023-04-03 19:11:18 +0200

    Add criterion benchmark for solver speed

---
+++ benches/search_speed.rs
@@ -0,0 +1,47 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use second_best::solver;
+
+pub fn solver_speed_begin(c: &mut Criterion) {
+    let mut solver = solver::Solver::default();
+    solver.be_quiet();
+    c.bench_function("solver speed (depth 7)", |b| {
+        b.iter(|| solver.search(black_box(7)))
+    });
+    c.bench_function("solver speed (depth 9)", |b| {
+        b.iter(|| solver.search(black_box(9)))
+    });
+    // c.bench_function("solver speed (depth 12)", |b| {
+    //     b.iter(|| solver.search(black_box(12)))
+    // });
+}
+
+pub fn solver_speed_end(c: &mut Criterion) {
+    let mut solver = solver::Solver::default();
+    solver.be_quiet();
+    // Position which is extremely symmetric, at the start of the second phase.
+    solver
+        .position
+        .parse_and_play_moves(
+            "0 1 2 3 4 5 6 7 1 2 3 4 5 6 7 0"
+                .split_whitespace()
+                .map(|s| s.to_string())
+                .collect(),
+        )
+        .unwrap();
+    c.bench_function("solver speed end (depth 7)", |b| {
+        b.iter(|| solver.search(black_box(7)))
+    });
+    c.bench_function("solver speed end (depth 9)", |b| {
+        b.iter(|| solver.search(black_box(9)))
+    });
+    // c.bench_function("solver speed end (depth 12)", |b| {
+    //     b.iter(|| solver.search(black_box(12)))
+    // });
+}
+
+criterion_group! {
+    name = benches;
+    config = Criterion::default().sample_size(50);
+    targets = solver_speed_begin, solver_speed_end
+}
+criterion_main!(benches);
--- benchmark_results.md
+++ benchmark_results.md
@@ -24,3 +24,13 @@ Average time: 0.0203s
 Average number of nodes searched: 93832.80
 Average knps: 4627.06 knps
 ``` 
+
+```terminal
+
+solver speed (depth 7)  time:   [34.095 ms 34.390 ms 34.756 ms]
+solver speed (depth 9)  time:   [272.22 ms 273.93 ms 275.98 ms]
+solver speed end (depth 7)
+                        time:   [77.575 ms 77.664 ms 77.763 ms]
+solver speed end (depth 9)
+                        time:   [2.3153 s 2.3237 s 2.3336 s]
+```
